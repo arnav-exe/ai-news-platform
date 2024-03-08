0aeb4eaaf220728345ea8a1bbea5abfd73beba8c
arnav-exe <91348707+arnav-exe@users.noreply.github.com>
2024-03-08 00:15:13 +0000

    Refactor articleScrape import and fix variable assignment

--- src/routes/scraper.js
+++ src/routes/scraper.js
@@ -1,12 +1,14 @@
-// import articles from "./Article.svelte";
-// import puppeteer from 'puppeteer-core';
+import articles from "./Article.svelte";
+import puppeteer from 'puppeteer-core';
 
 
-export const articleScrape =  _ => {
+export const articleScrape = _ => {
     // const browser = await puppeteer.launch();
     // const page = await browser.newPage();
 
 
-    // articles = json["articles"];
-    console.log("asdf");
+    articles = json["articles"];
+    console.log(articles);
 };
+
+export default articleScrape;
