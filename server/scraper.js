c3f67a7542aa83874c349b73805f9fec20f73c50
arnav-exe <91348707+arnav-exe@users.noreply.github.com>
2024-03-11 15:15:35 +0000

    Add article scraping functionality

--- server/scraper.js
+++ server/scraper.js
@@ -2,5 +2,19 @@ import puppeteer from "puppeteer";
 
 const scrapeArticle = async _ => {
     const browser = await puppeteer.launch();
-    
+    const page = await browser.newPage();
+
+    // random article link for testing
+    await page.goto("https://www.bbc.com/news/entertainment-arts-68415018");
+
+    let childElements = await page.evaluate(_ => {
+        const elements = Array.from(document.querySelectorAll('[data-component="text-block"]'));
+
+        return elements.map(e => e.innerText);
+    })
+    await browser.close();
+
+    return childElements;
 }
+
+export default scrapeArticle;
