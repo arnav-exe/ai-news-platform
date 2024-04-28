ad05717720becb8fc33f64016617c7daa3566f9c
arnav-exe <91348707+arnav-exe@users.noreply.github.com>
2024-04-28 14:02:36 +0100

    pulling article sources using articles.source.name instead of articles.source.id as it had many null values, automated renaming of static files accordingly

--- src/routes/Article.svelte
+++ src/routes/Article.svelte
@@ -5,7 +5,7 @@
     import json from './data.json';
 
     const API_KEY = import.meta.env.VITE_NEWSAPI_KEY;
-    const API_URL = `https://newsapi.org/v2/top-headlines?sources?apiKey=${API_KEY}`;
+    const API_URL = `https://newsapi.org/v2/top-headlines?language=en&apiKey=${API_KEY}`;
     
     let articles = [];
 
@@ -53,15 +53,35 @@
         fetchArticleBody(url);
     }
 
+    const fetchArticles = async _ => {
+        try {
+            let articleRes = await fetch(API_URL)
+            if (!articleRes.ok) {
+                throw new Error("Failed to fetch data.");
+            }
+            articles = await articleRes.json();
+        }
+        catch (error) {
+            console.log("Error. Failed to fetch articles from NewsAPI:", error)
+        }
+    }
+
     onMount(async _ => {
 
         // SCRAPE FROM API_URL HERE INSTEAD OF CALLING FROM DATA.JSON
         // CODE HERE
-
-        articles = json["articles"]; // dummy data imported from "data.json"
+        await fetchArticles();
+        if (!articles) {
+            console.log("Error. no articles fetched.")
+        }
+        console.log(articles.articles)
         
-        for (const article of articles) {
-            await fetchArticleImg(article.url);
+
+        // loop through articles to get thumbnails for all null values
+        for (const article of articles.articles) {
+            if (!articles.articles.urlToImage) {
+                await fetchArticleImg(article.url);
+            }
         }
     });
 
@@ -132,25 +152,35 @@
     
     <!-- article cards -->
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-8 m-8 justify-items-center items-center">
-        {#each articles as article}
-        <button on:click={_ => {articleFetchButtonHandler(article.url); openArticle(article)}}> <!-- Pass article data to openArticle function -->
-            <div class="card card-hover overflow-hidden max-w-2xl aspect-auto">
-                <header>
-                    <img src="{articleThumbnails[article.url]}" class="bg-black/50 w-full height-auto aspect-[16/9]" alt="article thumbnail" />
-                </header>
-                <div class="p-4 space-y-4">
-                    <img src="../../{article.source.id}.png" class="max-w-12 h-auto" alt="{article.source.name}">
-                    <h3 class="h3 text-left" data-toc-ignore>{article.title}</h3>
-                </div>
-                <hr class="opacity-50" />
-                <footer class="p-4 flex justify-start items-center space-x-4">
-                    <div class="flex-auto flex justify-between items-center">
-                        <small data-toc-ignore>By: {article.source.name}</small>
-                        <small>Published: {article.publishedAt.slice(0, 10)}</small>
+        {#if articles.articles && articles.articles.length > 0}
+            {#each articles.articles as article}
+            <button on:click={_ => {articleFetchButtonHandler(article.url); openArticle(article)}}> <!-- Pass article data to openArticle function -->
+                <div class="card card-hover overflow-hidden max-w-2xl aspect-auto">
+                    <header>
+                        {#if !article.urlToImage}
+                            <img src="{articleThumbnails[article.url]}" class="bg-black/50 w-full height-auto aspect-[16/9]" alt="article thumbnail" />
+                        {:else}
+                            <img src="{article.urlToImage}" class="bg-black/50 w-full height-auto aspect-[16/9]" alt="article thumbnail" />
+                        {/if}
+                    </header>
+                    <div class="p-4 space-y-4">
+                        {#if article.source.name}
+                            <img src="../../{article.source.name}.png" class="max-w-12 h-auto" alt="{article.source.name}">
+                        {/if}
+                        <h3 class="h3 text-left" data-toc-ignore>{article.title}</h3>
                     </div>
-                </footer>
-            </div>
-        </button>
-        {/each}
+                    <hr class="opacity-50" />
+                    <footer class="p-4 flex justify-start items-center space-x-4">
+                        <div class="flex-auto flex justify-between items-center">
+                            <small data-toc-ignore>By: {article.source.name}</small>
+                            <small>Published: {article.publishedAt.slice(0, 10)}</small>
+                        </div>
+                    </footer>
+                </div>
+            </button>
+            {/each}
+        {:else}
+        <p>LOADING...</p>
+        {/if}
     </div>
 </div>
